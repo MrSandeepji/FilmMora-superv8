7f798f7c64618b822fe92801ae151b5b99c07346
MrSandeepji <142229130+MrSandeepji@users.noreply.github.com>
2023-09-01 16:01:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -74,10 +74,10 @@ LAZY_YT_HANDLE = environ.get('LAZY_YT_HANDLE','SandeepXD7')  # WITHOUT @ [  add 
 MOVIE_GROUP_USERNAME = environ.get('MOVIE_GROUP_USERNAME', "+ziDKC-4D9bwyMTI1") #[ without @ ]
 
 # Url Shortner
-URL_MODE = is_enabled((environ.get("URL_MODE","False")), False)
-URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'gplinks.in') #Always use website url from api section 
-URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', 'dcde8f88d46e8ec7b55d0961f7db657c2ae24aa1')
-LZURL_PRIME_USERS = [int(lazyurlers) if id_pattern.search(lazyurlers) else lazyurlers for lazyurlers in environ.get('LZURL_PRIME_USERS', '').split()]
+URL_MODE = is_enabled((environ.get("URL_MODE","True")), True)
+URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'vipurl.in') #Always use website url from api section 
+URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '7023f7f2c1ab72181a53435284679dc67f5b9a7d')
+LZURL_PRIME_USERS = [int(lazyurlers) if id_pattern.search(lazyurlers) else lazyurlers for lazyurlers in environ.get('LZURL_PRIME_USERS', '978684334 5404010420 1608576332').split()]
 lazy_groups = environ.get('LAZY_GROUPS','')
 LAZY_GROUPS = [int(lazy_groups) for lazy_groups in lazy_groups.split()] if lazy_groups else None # ADD GROUP ID IN THIS VARIABLE
 my_users = [int(my_users) if id_pattern.search(my_users) else my_users for my_users in environ.get('MY_USERS', '').split()]
